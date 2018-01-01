2d6d11f89cbfe97d43e232def9d590d145da9d3b
Steffen André Langnes <steffenl.sw@gmail.com>
2018-01-01 23:21:27 +0100

    asd

--- scripts/container/run.sh
+++ scripts/container/run.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-pure-pw show "$FTP_USER" -f /etc/pure-ftpd/passwd/pureftpd.passwd > /dev/null \
-    || echo "$FTP_PASSWORD" | python /scripts/create-ftp-user.py "$FTP_USER" \
+pure-pw show "$FTP_USER" -f /etc/pure-ftpd/passwd/pureftpd.passwd 2>&1 > /dev/null \
+    || (echo "$FTP_PASSWORD" | python /scripts/create-ftp-user.py "$FTP_USER") \
     || exit 1
-
-/run.sh -c 5 -C 5 -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -P "$PUBLICHOST" -p 30000:30009 || exit 1
+chown -R ftpuser /home/ftpusers
+/run.sh -c 5 -C 5 -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -P "$FTP_WAN_HOST" -p 30000:30009 || exit 1
