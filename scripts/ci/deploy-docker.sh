cb2992e1f64eaf68a5af55ce5ef540138a45fa84
Steffen André Langnes <steffenl.sw@gmail.com>
2018-01-01 23:47:05 +0100

    asd

--- scripts/ci/deploy-docker.sh
+++ scripts/ci/deploy-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Uploading image..."
+echo "Uploading images..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || exit 1
-docker-compose push web || exit 1
+docker-compose push web ftp || exit 1
