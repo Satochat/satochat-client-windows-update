1cb2112612936181cf1101d5199699a00c10e7d5
Steffen André Langnes <steffenl.sw@gmail.com>
2018-01-02 00:37:24 +0100

    asd

--- scripts/ci/deploy-server.sh
+++ scripts/ci/deploy-server.sh
@@ -18,10 +18,10 @@ cat docker-compose.ci.prod.yml | envsubst | tee docker-compose.ci.prod.yml
 scp docker-compose.yml docker-compose.ci.prod.yml Dockerfile* "$DEPLOY_APPSERVER_SSH_HOST:$remoteTempDir" || exit 1
 
 echo "Stopping containers..."
-ssh "$DEPLOY_APPSERVER_SSH_HOST" "cd '$remoteTempDir' && docker-compose -p '$COMPOSE_PROJECT_NAME' -f docker-compose.yml -f docker-compose.ci.prod.yml down" || exit 1
+ssh "$DEPLOY_APPSERVER_SSH_HOST" "cd '$remoteTempDir' && docker-compose -f docker-compose.yml -f docker-compose.ci.prod.yml down" || exit 1
 
 echo "Running containers..."
-ssh "$DEPLOY_APPSERVER_SSH_HOST" "cd '$remoteTempDir' && docker-compose -p '$COMPOSE_PROJECT_NAME' -f docker-compose.yml -f docker-compose.ci.prod.yml up -d" || exit 1
+ssh "$DEPLOY_APPSERVER_SSH_HOST" "cd '$remoteTempDir' && docker-compose -f docker-compose.yml -f docker-compose.ci.prod.yml up -d" || exit 1
 
 # TODO: Run tests
 # TODO: Roll back on failure
